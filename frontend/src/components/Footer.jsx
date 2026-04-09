ecc0a4f6dc1bbab95830d0417771def99ee99792
nikazoro <101596449+nikazoro@users.noreply.github.com>
2026-04-09 10:03:01 +0530

    first commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
+      <p>
+        &copy; {new Date().getFullYear()} WriteFlow | Created with ❤️ by Suraj Singh Rana
+        Kumar
+      </p>
+    </footer>
+  );
+};
+export default Footer;
