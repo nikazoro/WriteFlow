4031b3fbf8429ff465258b1ee82d6371366820fe
nikazoro <101596449+nikazoro@users.noreply.github.com>
2026-04-09 11:17:10 +0530

    fix footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,6 @@ const Footer = () => {
     <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
       <p>
         &copy; {new Date().getFullYear()} WriteFlow | Created with ❤️ by Suraj Singh Rana
-        Kumar
       </p>
     </footer>
   );
